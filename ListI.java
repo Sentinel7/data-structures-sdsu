07afa6d1c92856819c090002bbdfc1a649ad91e5
Jason Thomo <amjbvic@gmail.com>
2018-12-19 18:21:03 -0800

    initial

---
+++ ListI.java
@@ -0,0 +1,9 @@
+
+public interface ListI<E> {
+	public void addFirst(E obj);		
+	public void addLast(E obj);
+	public E removeFirst();
+	public E removeLast();		
+	public E remove(E obj);
+	public boolean contains(E obj);
+}
