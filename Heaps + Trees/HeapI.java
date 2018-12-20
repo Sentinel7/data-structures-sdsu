20d2d840d76f06231c5d258dd216b010de375006
Jason Thomo <amjbvic@gmail.com>
2018-12-19 18:38:00 -0800

    Update HeapI.java

--- Heaps + Trees/HeapI.java
+++ Heaps + Trees/HeapI.java
@@ -1,5 +1,8 @@
 
 public interface HeapI<E> {
-
-	
+	public void add(E obj);
+	public E remove();
+	public void swap(int a, int b);
+	public void trickleUp(int position);
+	public void trickleDown(int parent);
 }
