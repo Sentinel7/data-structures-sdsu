29b9236457a557755ce32a026ee61bb81b1e6c3b
Jason Thomo <amjbvic@gmail.com>
2018-12-19 18:28:02 -0800

    Delete ListI.java

--- ListI.java
+++
@@ -1,9 +0,0 @@
-
-public interface ListI<E> {
-	public void addFirst(E obj);		
-	public void addLast(E obj);
-	public E removeFirst();
-	public E removeLast();		
-	public E remove(E obj);
-	public boolean contains(E obj);
-}
